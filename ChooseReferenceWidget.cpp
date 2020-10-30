29102ca3758978c11ea8b803397dd3d08f83208f
LorenDB <computersemiexpert@outlook.com>
2020-10-29 20:55:57 -0400

    Fix typo in 'Ecclesiastes'

--- ChooseReferenceWidget.cpp
+++ ChooseReferenceWidget.cpp
@@ -24,7 +24,7 @@ ChooseReferenceWidget::ChooseReferenceWidget(QWidget *parent)
 	      << "Joshua" << "Judges" << "Ruth" << "1 Samuel" << "2 Samuel"
 	      << "1 Kings" << "2 Kings" << "1 Chronicles" << "2 Chronicles" << "Ezra"
 	      << "Nehemiah" << "Esther" << "Job" << "Psalms" << "Proverbs"
-	      << "Ecclesiasties" << "Song of Solomon" << "Isaiah" << "Jeremiah"
+	      << "Ecclesiastes" << "Song of Solomon" << "Isaiah" << "Jeremiah"
 	      << "Lamentations" << "Ezekiel" << "Daniel" << "Hosea" << "Joel" << "Amos"
 	      << "Obadiah" << "Jonah" << "Micah" << "Nahum" << "Habakkuk"
 	      << "Zephaniah" << "Haggai" << "Zechariah" << "Malachi" << "Matthew"
