0e46906cbe8374351becc0bf3144ee87fe50aa4f
LorenDB <computersemiexpert@outlook.com>
2020-10-31 20:00:23 -0400

    Center window in screen at startup

--- main.cpp
+++ main.cpp
@@ -3,11 +3,16 @@
 
 #include <QApplication>
 #include <QPushButton>
+#include <QPoint>
+#include <QScreen>
+#include <QDebug>
 
 #include "MainWindow.h"
 #include "MemorizeWidget.h"
 #include "ChooseReferenceWidget.h"
 #include "Bible.h"
+
+inline QPoint centerWindowOnScreen(int, int);
 
 int main(int argc, char *argv[])
 {
@@ -15,7 +20,19 @@ int main(int argc, char *argv[])
 
 	MainWindow mainWindow;
 	mainWindow.setWindowTitle(QApplication::tr("TotalReqall Bible Memory"));
+
+	// TODO: Somebody with a Mac should figure out whether this is needed
+	mainWindow.setUnifiedTitleAndToolBarOnMac(true);
+
+	// calling move() before show() doesn't seem to work properly
 	mainWindow.show();
+	mainWindow.move(centerWindowOnScreen(mainWindow.width(), mainWindow.height()));
 
 	return a.exec();
+}
+
+inline QPoint centerWindowOnScreen(int w, int h)
+{
+	auto size = QApplication::primaryScreen()->geometry();
+	return QPoint{ (size.width() - w) / 2, (size.height() - h) / 2 };
 }
