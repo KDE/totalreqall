8b61c484f96b96e3f8dcd5447561e39b76d3dfd0
LorenDB <computersemiexpert@outlook.com>
2020-11-19 22:30:02 -0500

    Translations filled out
    I think that CMake did this for me automatically.

--- TotalReqall_en_US.ts
+++ TotalReqall_en_US.ts
@@ -1,4 +1,255 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="TotalReqall_en_US">
+<context>
+    <name>Bible</name>
+    <message>
+        <source>Failed to open :/resources/bible.txt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Loading Bible</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Freeing Bible</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ChooseReferenceWidget</name>
+    <message>
+        <source>Choose a verse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Memorize verse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Display verse</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Settings...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Online help...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About maddy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Changelog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>maddy is a C++ Markdown to HTML &lt;b&gt;header-only&lt;/b&gt; parser library.&lt;br&gt;&lt;br&gt;&lt;a href=&quot;https://github.com/progsource/maddy&quot;&gt;https://github.com/progsource/maddy&lt;/a&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Couldn&apos;t load file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The file </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> could not be opened.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> version </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;br&gt;Copyright © 2020 Loren Burkholder.&lt;br&gt;&lt;br&gt;&lt;a href=&quot;https://lorendb.github.io/TotalReqall&quot;&gt;https://lorendb.github.io/TotalReqall&lt;/a&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please choose a verse.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MarkdownDialog</name>
+    <message>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MemorizeEdit</name>
+    <message>
+        <source>Oops, you messed up! Try again.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Oops, you messed up!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Done!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MemorizeWidget</name>
+    <message>
+        <source>Stop memorizing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Continue</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QApplication</name>
+    <message>
+        <source>TotalReqall Bible Memory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>TotalReqall</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Retype the word</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mark the word as incorrect and &amp;keep going</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Load &amp;last verse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Load &amp;random verse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Load a &amp;set verse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Choose verse...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save last set window size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Swap Memorize and Display buttons</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This will take effect after a restart.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Reset all settings...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Set action when the wrong key is typed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Set what verse to load on startup</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Confirm reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Are you sure you want to reset all settings? This will take effect immediately.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Memorization</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Display</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Startup</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Other settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Restart program</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The buttons will be swapped after the program is restarted.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SimpleRefChooser</name>
+    <message>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
